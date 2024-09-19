131a502e410f0639f0ca6401dbb26df02ede2bd4
AnjaliS4 <anjalisimkhada5@gmail.com>
2024-09-19 22:31:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-let display = document.getElementById('display');
-function appendDigit(digit) {
-    display.value += digit;
-}
-function appendOperator(op) {
-    display.value += op;
-}
-function calculateResult() {
-    display.value = eval(display.value);
-}
-function clearDisplay() {
-    display.value = '';
-}
-function deleteDigit() {
-    display.value = display.value.slice(0, -1);
-}
+
